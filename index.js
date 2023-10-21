5dca873791dbe362f6400844e1852e9545e4b745
Md Noble <mdnoble.wd@gmail.com>
2023-10-21 15:21:18 +0600

    filterd and fetched data from database to show details

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
-
-// carzaneDB
-// qm2pRFGJANnNer6Q
-
 // console.log(process.env.DB_USER)
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.crviidq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,36 +27,48 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const brandsCollection = client.db('carzane').collection('brands');
-    const productsCollection = client.db('carzane').collection('products');
+    const brandsCollection = client.db("carzane").collection("brands");
+    const productsCollection = client.db("carzane").collection("products");
 
-    app.get('/brands', async (req, res) => {
-      const cursor = brandsCollection.find()
+    app.get("/brands", async (req, res) => {
+      const cursor = brandsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.post('/products' , async(req , res) => {
-        const newProduct = req.body;
-        const result = await productsCollection.insertOne(newProduct);
-        res.send(result);
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    app.get("/products/:id" , async(req , res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.get("/products?:id" , async(req , res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.findOne(query);
+      res.send(result);
     })
 
 
-
-
-
-
-
-
-
-
-
-
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -72,14 +76,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("CARZANE server is running");
+});
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('CARZANE server is running');
-  });
-  
-  app.listen(port, () => {
-    console.log(`CARZANE server running on ${port}`);
-  })
+app.listen(port, () => {
+  console.log(`CARZANE server running on ${port}`);
+});
